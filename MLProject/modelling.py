7fd49ffad1a2e6d66f7d4f492c14424a929e85cf
imanrohiman <35205413+imanrohiman@users.noreply.github.com>
2026-06-26 09:26:39 +0700

    update modell.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -11,8 +11,11 @@ import warnings
 warnings.filterwarnings('ignore')
 
 # ❌ matikan autolog biar tidak crash di CI
-# mlflow.autolog()
-mlflow.autolog(disable=True)
+# mlflow.autolog(disable=True)
+
+# Aktifkan autolog untuk Kriteria Basic
+mlflow.sklearn.autolog()
+
 
 
 def load_and_preprocess_data():
@@ -20,20 +23,10 @@ def load_and_preprocess_data():
 
     X = df.drop('Survived', axis=1)
     y = df['Survived']
-
-    le = LabelEncoder()
-    categorical_cols = X.select_dtypes(include=['object']).columns
-
-    for col in categorical_cols:
-        X[col] = le.fit_transform(X[col].astype(str))
-
+    
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42, stratify=y
     )
-
-    scaler = StandardScaler()
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.transform(X_test)
 
     return X_train, X_test, y_train, y_test, scaler
 
